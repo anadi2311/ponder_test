38742774c59c1e184ab62a9a06f6c05328d17454
Anadi <47948789+anadi2311@users.noreply.github.com>
2023-09-18 14:11:56 -0700

    ponder first commit

--- src/TransparentUpgradeableProxy.ts
+++ src/TransparentUpgradeableProxy.ts
@@ -1,1 +1,16 @@
 import { ponder } from "@/generated";
+
+ponder.on("TransparentUpgradeableProxy:Supply", async ({ event, context }) => {
+  const { Pool } = context.entities;
+
+  // Concatenate the transaction hash and log index to create a unique ID
+  const uniqueID = `${event.transaction.hash}-${event.log.logIndex}`;
+
+  await Pool.create({
+    id: uniqueID, // Use the unique ID
+    data: {
+      address: event.params.from,
+      supplyAmount: event.params.amount,
+    },
+  });
+});
